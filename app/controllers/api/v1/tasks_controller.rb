b7fce0ae7de39732449ccf50b678e4c00bbf8bf8
Ndelforno <nicolas.delforno@gmail.com>
2019-01-23 15:09:18 -0500

    api endpoint for creating tsk completed

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,8 +1,21 @@
 module Api::V1
   class TasksController < ApplicationController
+
     def index
       @tasks = Task.all
       render json: @tasks
     end
+
+    def create
+      @task = Task.create(task_params)
+      render json: @task
+    end
+
+    private
+
+    def task_params
+      params.require(:task).permit(:title, :body)
+    end
+
   end
 end
